ff9f25e1d68e81692470741d52426c72294fb708
Oladipupo <oladipupobankole24@gmail.com>
2024-09-16 21:43:21 +0100

    Refactor shopping cart functionality to use object-oriented programming

--- js/script.js
+++ js/script.js
@@ -1,10 +1,90 @@
-const plus = document.querySelectorAll(".fa-plus-circle");
+// OOP Shopping Cart
+class ShoppingCart {
+    constructor() {
+        this.plus = document.querySelectorAll(".fa-plus-circle");
+        this.minus = document.querySelectorAll(".fa-minus-circle");
+        this.del = document.querySelectorAll(".fa-trash-alt");
+        this.like = document.querySelectorAll(".fa-heart");
+        this.quantity = document.querySelectorAll(".quantity");
+        this.price = [100, 20, 50];
+        this.cartTotal = [];
+        this.cart = document.querySelectorAll(".cart");
+        this.result = document.querySelector(".total");
+    }
+
+    addQuantity() {
+        const add = Array.from(this.plus);
+        for (let i = 0; i < add.length; i++) {
+            add[i].addEventListener("click", () => {
+                this.quantity[i].textContent++;
+                this.cartTotal.push(this.price[i]);
+                const sum = this.cartTotal.reduce((a, b) => a + b, 0);
+                this.result.textContent = `${sum} $`;
+            });
+        }
+    }
+
+    subQuantity() {
+        const sub = Array.from(this.minus);
+        for (let i = 0; i < sub.length; i++) {
+            sub[i].addEventListener("click", () => {
+                if (this.quantity[i].textContent > 0) {
+                    this.quantity[i].textContent--;
+                    this.cartTotal.splice(this.cartTotal.indexOf(this.price[i]), 1);
+                    const sum = this.cartTotal.reduce((a, b) => a + b, 0);
+                    this.result.textContent = `${sum} $`;
+                }
+            });
+        }
+    }
+
+    delQuantity() {
+        for (let i = 0; i < this.del.length; i++) {
+            this.del[i].addEventListener("click", () => {
+                this.cart[i].remove();
+                this.del.length--;
+                this.cartTotal.splice(
+                    this.cartTotal.indexOf(this.price[i]),
+                    this.cartTotal.filter((item) => item == this.price[i]).length
+                );
+                this.result.textContent = `${this.cartTotal.reduce(
+                    (a, b) => a + b,
+                    0
+                )} $`;
+                if (this.del.length == 0) {
+                    location.reload();
+                }
+            });
+        }
+    }
+
+    likeReaction() {
+        for (let i = 0; i < this.like.length; i++) {
+            this.like[i].addEventListener("click", () => {
+                this.like[i].style.color = "red";
+            });
+            this.like[i].addEventListener("dblclick", () => {
+                this.like[i].style.color = "black";
+            });
+        }
+    }
+}
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addQuantity();
+shoppingCart.subQuantity();
+shoppingCart.delQuantity();
+shoppingCart.likeReaction();
+
+
+// Functional Programming Shopping Cart
+/* const plus = document.querySelectorAll(".fa-plus-circle");
 const minus = document.querySelectorAll(".fa-minus-circle");
 const del = document.querySelectorAll(".fa-trash-alt");
 const like = document.querySelectorAll(".fa-heart");
 const quantity = document.querySelectorAll(".quantity");
-let result = document.querySelector(".total");
 let price = [100, 20, 50];
+const result = document.querySelector(".total");
 let cartTotal = []
 
 const cart = document.querySelectorAll(".cart");
@@ -59,4 +139,4 @@ function likeReaction(like) {
         });
     }
 }
-likeReaction([...like]);
+likeReaction([...like]); */
